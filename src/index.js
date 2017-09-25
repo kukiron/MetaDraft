3425197a6cc10838822d573e30c3d4776f552c53
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2017-09-25 21:27:52 +0600

    Rename top level component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MyTextEditor from './MyTextEditor';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 
-ReactDOM.render(<MyTextEditor />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
