0a6168c70b51c6b3b51db36a8a20dbf4529aef01
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2017-09-25 21:28:29 +0600

    Renamed css file

--- src/MyTextEditor.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MyTextEditor from './MyTextEditor';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MyTextEditor />, div);
-});
